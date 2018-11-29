78abfdaadfcfab5b7a738891deede4a38030c1a2
Keagan Jarvis <keaganjarvis@gmail.com>
2018-11-29 15:40:42 +0200

    Full EPC toplogy setup and file renaming

--- containernet_example2.py
+++ containernet_example2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 """
-This is the most simple example to showcase Containernet.
+This is the EPC implementation inside container net
 """
 from mininet.net import Containernet
 from mininet.node import RemoteController
@@ -9,23 +9,30 @@ from mininet.link import TCLink
 from mininet.log import info, setLogLevel
 setLogLevel('info')
 
-net = Containernet(controller=RemoteController,autoSetMacs=False)
+net = Containernet(controller=RemoteController)
 info('*** Adding controller\n')
-net.addController('c0')
+c0 = net.addController('c0', ip='172.17.0.1', port=6633)
 info('*** Adding docker containers\n')
-hss = net.addDocker('hss', ip='10.0.0.251', mac='11:11:11:11:11:11', dimage="ubuntu:trusty") #33:33:00:00:00:fb
-mme = net.addDocker('mme', ip='10.0.0.252', mac='22:22:22:22:22:22', dimage="ubuntu:trusty") #fe:e4:1c:1b:df:78
+hss = net.addDocker('hss', ip='10.0.0.251', dimage="ssh:latest")
+comp = net.addDocker('comp', ip='10.0.0.252', dimage="ssh:latest")
+mme = net.addDocker('mme', ip='10.0.0.253', dimage="ssh:latest")
+spgw = net.addDocker('spgw', ip='10.0.0.254', dimage="ssh:latest")
+
 info('*** Adding switches\n')
 s1 = net.addSwitch('s1')
-s2 = net.addSwitch('s2')
+# s2 = net.addSwitch('s2')
 info('*** Creating links\n')
 net.addLink(hss, s1)
-net.addLink(s1, s2, cls=TCLink, delay='100ms', bw=1)
-net.addLink(s2, mme)
+# net.addLink(s1, s2, cls=TCLink, delay='100ms', bw=1)
+net.addLink(s1, comp)
+net.addLink(s1, mme)
+net.addLink(s1, spgw)
 info('*** Starting network\n')
 net.start()
 info('*** Testing connectivity\n')
 net.ping([hss, mme])
+net.ping([spgw, mme])
+net.ping([comp, mme])
 info('*** Running CLI\n')
 CLI(net)
 info('*** Stopping network')
